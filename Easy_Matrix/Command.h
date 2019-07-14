d9924ed59ff07d3c34853bd4bd54fabc74ee3e1d
friendming <qm760681623@163.com>
2019-07-14 20:45:30 +0800

    命令匹配初步成型，但是不可用

---
+++ Easy_Matrix/Command.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include<cstring>
+#include<iostream>
+
+using namespace std;
+/////////////////////
+char Command[20][100];
+int commandPlace[100]={-1};
+/////////////////////
+int getCommand(char incom[100]);
+int pipeiCommand(char incom[100], char pipeiCom[100]);//指令匹配函数 目前只支持一行中 一种指令只出现一次
+/////////////////////
+int getCommand(char incom[100]) {
+	//压缩空格：多个空格合为一个
+	int spaceTimes = 0;
+	for (int i = 0; incom[i] != 0; i++) {
+		if (incom[i] == 32) {//空格
+			spaceTimes++;
+		}
+		else {
+			if (spaceTimes >= 2) {//他前面有好几个空格的话
+				for (int j = i-spaceTimes; incom[j-1]!=0; j++) {//后面的填补一波到前面去
+					incom[j] = incom[j + spaceTimes];
+				}
+			}
+		}
+	}
+	///压缩空格结束
+	cout << incom;////////////////////////////////////////////////////////////TEST!!!!!!
+	for (int com = 0; com < 20; com++) {//逐个指令依次匹配
+		int place = pipeiCommand(incom, Command[com]);//返回匹配位置
+		if (place != -1) {//如果匹配上了
+			commandPlace[place] = com;//在相应的位置（该指令在字符串中字符的位置）记录匹配到了第几个指令//之后进行压缩！！！
+		}
+	}
+	return 0;
+}
+
+int pipeiCommand(char incom[100], char pipeiCom[100]) {//指令匹配函数 目前只支持一行中 一种指令只出现一次
+	int lenIncom=0, lenPipeiCom=0;
+	for (int i = 0; incom[i] != 0; i++) {
+		lenIncom++;
+	}
+	for (int i = 0; pipeiCom[i] != 0; i++) {
+		lenPipeiCom++;
+	}
+	int i, j;
+	for (i = 0; i <= lenIncom - lenPipeiCom; ++i) {
+		for (j = 0; j < lenPipeiCom; ++j) {
+			if (incom[i + j] != pipeiCom[j]) {
+				break;
+			}
+		}
+		if (j == lenPipeiCom) {
+			return i;//返回指令的首字母位置
+		}
+	}
+	return -1;
+}
--- Easy_Matrix/Main.cpp
+++ Easy_Matrix/Main.cpp
@@ -1,7 +1,14 @@
 #include<iostream>
+#include"Command.h"
 
 using namespace std;
 
 int main() {
+	char inputCommand[100];
+	cout << "Easy Matrix|You can solve easy matrix problems easily!|Produced by QM" << endl;
+	cout << "Enter \"set language = English\"to change language." << endl;
+	cin >> inputCommand;
+	getCommand(inputCommand);
+
 	return 0;
 }
